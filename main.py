dc3e057b6ea29815f3505fb4d65620e9ede525ae
plexussolaris <jasper.smit1@gmail.com>
2026-08-18 09:21:22 +0200

    Added function for paraphrasing rather than clustering

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from sentence_transformers import SentenceTransformer
+from sentence_transformers.util import paraphrase_mining
 from hdbscan import HDBSCAN
 from sklearn.cluster import KMeans
 from umap import UMAP
 import numpy as np
 import pandas as pd
-from typing import List
 
 def embed_elements(elements:pd.Series) -> tuple[pd.Series, np.ndarray]:
     text = elements.reset_index(drop=True)
@@ -24,6 +24,20 @@ def cluster_hdbscan(vectors:np.ndarray) -> np.ndarray:
     reduced = UMAP(n_components=10, metric='cosine', random_state=42).fit_transform(vectors)
     hdbscan = HDBSCAN(metric='euclidean')
     return hdbscan.fit_predict(reduced)
+
+
+def paraphrase_miner(elements:pd.Series) -> pd.DataFrame:
+    model = SentenceTransformer('all-MiniLM-L6-v2')
+    sentences = elements.to_list()
+    paraphrases = paraphrase_mining(model, sentences)
+    for paraphrase in paraphrases[0:10]:
+        score, i, j = paraphrase
+        print("{} \t\t {} \t\t Score: {:.4f}".format(sentences[i], sentences[j], score))
+
+    df = pd.DataFrame(paraphrases, columns=['score', 'i', 'j'])
+    df['sentence_1'] = df['i'].map(lambda idx: sentences[idx])
+    df['sentence_2'] = df['j'].map(lambda idx: sentences[idx])
+    return df[['sentence_1', 'sentence_2', 'score']]
 
 
 def main():
@@ -47,8 +61,12 @@ def main():
             'hdbscan_cluster_id': cluster_hdbscan(vectors),
         })
 
+    paraphrased = {}
+    for entity, series in to_transform.items():
+        paraphrased[entity] = paraphrase_miner(series)
+
     for entity, df in clustered.items():
-        df.to_excel(f'outputs/{entity}_clustered.xlsx')
+        df.to_excel(f'outputs/{entity}_clustered.xlsx', index=False)
 
 if __name__ == "__main__":
     main()
